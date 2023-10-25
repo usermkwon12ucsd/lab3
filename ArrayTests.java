f58c0b3a5804a9ee3889833e9b89362117ada968
MK013467 <73623570+MK013467@users.noreply.github.com>
2023-10-25 16:42:50 -0700

    ArrayExample fixed

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,24 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = {1,2};
+
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{2,1}, input2);
+
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = {1,2};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{2,1}, ArrayExamples.reversed(input2));
+
   }
+
+  
 }
